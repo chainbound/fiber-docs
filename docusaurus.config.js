4f92ed2a8a13d894b4f60f3d32921142c4b10197
Jonas Bostoen <jonas.bostoen@hotmail.com>
2022-10-14 14:05:23 +0200

    chore: fix netlify deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,16 +8,12 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Fiber Network',
   tagline: 'Global high speed network for Ethereum transactions',
-  url: 'https://chainbound.github.io',
-  baseUrl: '/website/',
+  url: 'https://fiber.chainbound.io',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'chainbound', // Usually your GitHub org/user name.
-  projectName: 'website', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
