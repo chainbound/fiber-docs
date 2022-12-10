161292ae4af876da00950b7844b9188ddffc0458
Jonas Bostoen <jonas.bostoen@hotmail.com>
2022-12-10 12:12:51 +0100

    feat(demo): more info in table

--- src/pages/demo.tsx
+++ src/pages/demo.tsx
@@ -31,8 +31,8 @@ export default class DemoTable extends React.Component<{}, { transactions: Array
 
         this.socket.addEventListener('message', (msg) => {
             const txEvent = JSON.parse(msg.data as string);
-            if (!this.state.transactions.some(tx => tx.Hash === txEvent.Hash)) {
-                this.setState({ transactions: [txEvent, ...this.state.transactions.slice(0, 9)] });
+            if (!this.state.transactions.some(tx => tx.Hash === txEvent.Hash) && !txEvent.From.includes('fiber-node')) {
+                this.setState({ transactions: [txEvent, ...this.state.transactions.slice(0, 40)] });
             }
         });
 
@@ -46,15 +46,19 @@ export default class DemoTable extends React.Component<{}, { transactions: Array
             <Layout title="Demo">
                 <div className={clsx(styles.demo)}>
                     <h1>Demo</h1>
-                    <p>This is a live table of Uniswap trades recorded from the Fiber transaction stream, with data about
-                        when and where the transaction was seen in the network.
+                    <p>This is a live table of the <a target="_blank" href="https://etherscan.io/address/0x68b3465833fb72a70ecdf485e0e4c7bd8665fc45">Uniswap Swap Router</a> trades recorded from the Fiber transaction stream, with data about
+                        when and where the transaction was seen in the network. <b>The stream will have a delay of 10 seconds, because to get the
+                        extra metadata for the transaction, we need to wait for it to be indexed in the database.</b> The actual moment
+                        the transaction was seen by Fiber is shown in the timestamp column.
                     </p>
-                    <table>
+                    <table className={clsx(styles.table)}>
                         <thead>
                             <tr>
                                 <th>Hash</th>
-                                <th>Timestamp</th>
+                                <th>Timestamp (UTC)</th>
                                 <th>Location</th>
+                                <th>Action</th>
+                                <th>From Host</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -63,11 +67,15 @@ export default class DemoTable extends React.Component<{}, { transactions: Array
 
                                 parts = parts.slice(-3)
                                 let nodeID = parts.join('-')
+
+                                let host = txEvent.From.split('@')[1]
                                 return (
                                     <tr key={txEvent.Hash}>
                                         <td><a href={`https://etherscan.io/tx/${txEvent.Hash}`} target="_blank">{txEvent.Hash}</a></td>
-                                        <td>{txEvent.Timestamp}</td>
+                                        <td>{txEvent.Timestamp.split('+')[0]}</td>
                                         <td>{nodeID}</td>
+                                        <td>{txEvent.Action}</td>
+                                        <td>{host}</td>
                                     </tr>
                                 )
                             })}
