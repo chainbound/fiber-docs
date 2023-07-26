4c6bd5431da263f3908d028ea5d3f3a1821b59d5
Jonas Bostoen <jonas.bostoen@hotmail.com>
2023-07-26 19:24:45 +0200

    stats -> stats (coming soon)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -71,7 +71,7 @@ const config = {
           },
           { to: '/blog', label: 'Blog', position: 'left' },
           { to: '/pricing', label: 'Pricing', position: 'left' },
-          { href: 'http://fiber-stats.chainbound.io/public-dashboards/f82cf92d44dc439b811094a7c1b3eb43?orgId=1&refresh=30s', label: 'Stats', position: 'left' },
+          { to: '#', label: 'Stats (Coming Soon)', position: 'left' },
         ],
       },
       footer: {
